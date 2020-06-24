ce1e9839084f44700da174347f1d3c73e064a090
markkmwaura <markkmwaura@gmail.com>
2020-06-25 01:13:28 +0300

    using reacts context api & no third parties

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 	return (
 		<div className='card text-center'>
 			<img
 				src={avatar_url}
+				alt=''
 				className='round-img'
 				style={{ width: '60px' }}
-				alt=''
 			/>
 			<h3>{login}</h3>
 			<div>
@@ -20,8 +20,9 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 		</div>
 	);
 };
+
 UserItem.propTypes = {
-  user:PropTypes.object.isRequired
-}
+	user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
